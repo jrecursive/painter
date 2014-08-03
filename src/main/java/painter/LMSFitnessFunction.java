c33a4d550b1968d4835f4a80ee1c9f7fb3ed54c3
John Muellerleile <jmuellerleile@gmail.com>
2014-08-03 13:55:57 -0700

    license

--- src/main/java/painter/LMSFitnessFunction.java
+++ src/main/java/painter/LMSFitnessFunction.java
@@ -1,5 +1,22 @@
 /*
- * BASED ON http://www.java2s.com/Open-Source/Java/Development/jgap-3.4.4/examples/gp/monalisa/core/LMSFitnessFunction.java.htm
+ * Painter
+ * Copyright (c) 2014, John Muellerleile, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ *
+ * This code is based upon code in the JGAP library:
+ *  http://www.java2s.com/Open-Source/Java/Development/jgap-3.4.4/examples/gp/monalisa/core/LMSFitnessFunction.java.htm
  */
 package painter;
 
